4d7f9fac299536a0839dd935b5a05c85cd8c2b9d
0xCAF2 <pub@0xcaf2.dev>
2022-07-16 20:35:05 +0900

    tried patterns of assignment

--- script/converter.ts
+++ script/converter.ts
@@ -10,10 +10,36 @@ let sourceFile = ts.createSourceFile(
   true
 )
 
+let o = {}
+o = {}
+Reflect.get(o, 'toString')
+
 for (const stmt of sourceFile.statements) {
   if (ts.isVariableStatement(stmt)) {
-    const isConst = (stmt.declarationList.flags & ts.NodeFlags.Const) !== 0
     const name = stmt.declarationList.declarations[0].name.getText()
-    console.log(name, isConst)
+    const isConst = (stmt.declarationList.flags & ts.NodeFlags.Const) === 2
+    if (isConst) {
+      console.log('Const')
+      continue
+    } else {
+      console.log('Let')
+      continue
+    }
+  } else if (ts.isExpressionStatement(stmt)) {
+    if (
+      stmt
+        .getChildren()
+        .some(
+          (n) =>
+            n.kind === ts.SyntaxKind.BinaryExpression &&
+            n.getChildAt(1).kind === ts.SyntaxKind.FirstAssignment
+        )
+    ) {
+      console.log('Assign')
+      continue
+    } else {
+      console.log('Expr')
+      continue
+    }
   }
 }
